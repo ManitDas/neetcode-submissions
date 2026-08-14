3803861f86704b7d08608e09295ab2e8aa586f6b
Manit Das <manit.das123@gmail.com>
2026-08-14 15:43:00 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.values = []
+        self.mins = []
+
+    def push(self, val: int) -> None:
+        self.values.append(val)
+        if not self.mins:
+            self.mins.append(val)
+        else:
+            self.mins.append(min(val, self.mins[-1]))
+
+    def pop(self) -> None:
+        self.values.pop()
+        self.mins.pop()
+
+    def top(self) -> int:
+        return self.values[-1]
+
+    def getMin(self) -> int:
+        return self.mins[-1]
